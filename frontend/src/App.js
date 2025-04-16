e9dd38ff09c9cb213d009477b547bc148ee24796
Louisse Andre Dacuycuy <loosegoose@Louisses-MacBook-Air.local>
2025-04-15 23:39:44 -0400

    imoprted About page to current App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Navigate } from "react-router-dom"; 
 import Home from "./pages/Home";
+import About from "./pages/About"
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
@@ -22,6 +23,7 @@ function App() {
       <Navbar isAuthenticated={isAuthenticated} />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route
